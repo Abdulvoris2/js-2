1cff199dc68791715044d79f121c6e85f07cbd36
Abdulvoris2 <abdulvoris.aguda@gmail.com>
2025-07-22 21:27:27 +0500

    Add files via upload

---
+++ javaskript.js
@@ -0,0 +1,7 @@
+let user = prompt('sting typedagi malumot kiriting')
+let femi = prompt('Nomber typendagi malumot kiriting')
+let year = prompt('boolean typendagi malumot kiriting')
+prompt("hamma malumotlar to'g'ri kiritkanozga ishingiz komilmi ?")
+alert('siz kirgizgan sozi typedagi tegishli '   + user + 
+    'siz kirgizgan sozi nombr typedagi tegishli ' + femi  + 
+    'siz kirgizgan sozi boolean typedagi tegishli ' + year )
